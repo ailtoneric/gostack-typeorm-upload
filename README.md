6ec4b12d9d5f11d9fa051df9c5cb79e3a167c7b0
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2020-06-30 20:28:34 -0300

    create import services

---
+++ README.md
@@ -0,0 +1,36 @@
+## GoStack Challenges - Level 02
+Project developed during Rocketseat GoStack, Level 2 challenge on node fundamentals.
+
+<img alt="node-fundamentals" src="https://repository-images.githubusercontent.com/274746672/95c5fa80-b645-11ea-92cc-9595b4b2e1cc" />
+
+<p align="center">
+
+  <a href="LICENSE" style="text-decoration: none">
+    <img alt="License" src="https://img.shields.io/github/license/ailtoneric/gostack-fundamentals-node" />
+  </a>
+
+  <a href="#" style="text-decoration: none">
+    <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/ailtoneric/gostack-fundamentals-node" />
+  </a>
+
+</p>
+
+## :trophy: Technologies
+
+This project was developed with the following technologies:
+
+- [Node JS](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [Typescript](https://www.typescriptlang.org/)
+
+## :memo: License
+
+This project is under the MIT license. See the file [LICENSE](LICENSE) for more details.
+
+## :space_invader: Installation
+
+Run yarn to install the dependencies.
+
+---
+
+Developed by Ailton Oliveira :hear_no_evil::speak_no_evil::see_no_evil:
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,86 @@
+import { getRepository, getCustomRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransactions {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const categoryRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      delimiter: ',',
+      from_line: 2,
+    });
+
+    const parserCSV = contactsReadStream.pipe(parsers);
+
+    const transactions: CSVTransactions[] = [];
+    const categories: string[] = [];
+
+    parserCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parserCSV.on('end', resolve));
+
+    const existsCategories = await categoryRepository.find({
+      where: { title: In(categories) },
+    });
+
+    const existsCategoriesTitles = existsCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategoryTitles = categories
+      .filter(category => !existsCategoriesTitles.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoryRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoryRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existsCategories];
+
+    const createdTransactions = transactionsRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepository.save(createdTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions;
   }
 }
 
