ee284f441bacf5d4342900bbdfe9bba0349b102d
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2020-06-30 09:21:37 -0300

    create transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,61 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_id: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+
+    const typeIsValid = ['income', 'outcome'].includes(type);
+
+    if (!typeIsValid) {
+      throw new AppError('Type is not valid');
+    }
+
+    let categoryId: string;
+
+    const categoryRepository = getRepository(Category);
+
+    const categoryExists = await categoryRepository.findOne({
+      where: { title: category_id },
+    });
+
+    if (!categoryExists) {
+      const category = categoryRepository.create({
+        title: category_id,
+      });
+
+      await categoryRepository.save(category);
+
+      categoryId = category.id;
+    } else {
+      categoryId = categoryExists.id;
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      categoryId,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
